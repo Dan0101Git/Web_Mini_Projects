5a58113d5897e64eb09cb3a6fd5c8153f20e6229
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2025-09-10 18:36:57 +0530

    recursive solution 2

--- javascript/Data-Structures/Recursion/script.js
+++ javascript/Data-Structures/Recursion/script.js
@@ -1,13 +1,17 @@
 console.log("we are gpnna recurse, recur.., recu..,rec,..re...,r..");
 
-function fibs(n,arr=[0,1]){
+function fibs(n){
+console.log("This was printed recursively");
+if(n==2)
+    return [0,1]
+if(n<=1)
+    return [0]
 
-    if(n<=2)
-        return {result:n-1,arr};
 
-    const result= fibs(n-2,arr).result+fibs(n-1,arr).result;
-    if(n>arr.length)
-    arr.push(result);
-    return {result,arr}
+    let arr=fibs(n-1);
+arr.push(arr[arr.length-1]+arr[arr.length-2]);
+return arr;
+
 }
-console.log(fibs(2).arr);
+console.log(fibs(8));
+
